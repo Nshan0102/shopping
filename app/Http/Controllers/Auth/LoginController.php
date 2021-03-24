2438d2622752612d2ee8700022d25676d60b7562
Nshan Vardanyan <Nshan.Vardanyan.1996@gmail.com>
2021-03-25 00:37:53 +0400

    modify auth methods to use username

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -21,6 +22,8 @@ class LoginController extends Controller
 
     use AuthenticatesUsers;
 
+    protected $username;
+
     /**
      * Where to redirect users after login.
      *
@@ -36,5 +39,44 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+
+        $this->username = $this->findUsername();
     }
+
+    /**
+     * Get the login username to be used by the controller.
+     */
+    public function findUsername(): string
+    {
+        $login = request()->login;
+        $fieldType = filter_var($login, FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
+        request()->merge([$fieldType => $login]);
+        return $fieldType;
+    }
+
+    /**
+     * Get username property.
+     */
+    public function username(): string
+    {
+        return $this->username;
+    }
+
+    private function getValidationRules($key): string
+    {
+        $rules = [
+            'email' => 'required|email|exists:users',
+            'username' => 'required|string|exists:users'
+        ];
+        return $rules[$key];
+    }
+
+    protected function validateLogin(Request $request)
+    {
+        $request->validate([
+            $this->username => $this->getValidationRules($this->username),
+            'password' => 'required|string',
+        ]);
+    }
+
 }
