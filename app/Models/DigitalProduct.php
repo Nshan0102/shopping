26af170773055747b8858179b222ab51ae453fa5
Nshan Vardanyan <Nshan.Vardanyan.1996@gmail.com>
2021-03-25 22:44:37 +0400

    Implement ProductInterface Interface, use HasFactory trait, add fillable property

--- app/Models/DigitalProduct.php
+++ app/Models/DigitalProduct.php
@@ -2,11 +2,19 @@
 
 namespace App\Models;
 
+use App\Interfaces\ProductInterface;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
 
-class DigitalProduct extends Model
+class DigitalProduct extends Model implements ProductInterface
 {
+    use HasFactory;
+
+    protected $fillable = [
+        'platform'
+    ];
+
     public function product(): MorphOne
     {
         return $this->morphOne(Product::class, 'productable');
--- app/Models/PhysicalProduct.php
+++ app/Models/PhysicalProduct.php
@@ -2,12 +2,21 @@
 
 namespace App\Models;
 
+use App\Interfaces\ProductInterface;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
 
-class PhysicalProduct extends Model
+class PhysicalProduct extends Model implements ProductInterface
 {
+    use HasFactory;
+
+    protected $fillable = [
+        'dimension',
+        'color',
+        'free_shipping'
+    ];
+
     public function product(): MorphOne
     {
         return $this->morphOne(Product::class, 'productable');
