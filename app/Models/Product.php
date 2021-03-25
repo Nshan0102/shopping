e6b49f85de9896f61363b9b948a5f0fe5930a779
Nshan Vardanyan <Nshan.Vardanyan.1996@gmail.com>
2021-03-25 22:42:12 +0400

    Remove Interface implementation from Product and use HasFactory trait

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
-use App\Interfaces\ProductInterface;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 
-class Product extends Model implements ProductInterface
+class Product extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'name',
         'slug',
