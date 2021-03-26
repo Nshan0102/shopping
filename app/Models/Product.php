0aeac9d672adaa1f9f68ebb2dfef85ba68ff28e0
Nshan Vardanyan <Nshan.Vardanyan.1996@gmail.com>
2021-03-26 05:01:17 +0400

    add new methods in Product model getViewPartialPath, getCategory, getCategoryClass

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -21,4 +21,29 @@ class Product extends Model
     {
         return $this->morphTo();
     }
+
+    public function getViewPartialPath(): string
+    {
+        $type = explode('\\', $this->productable_type);
+        return '_partials.product.'.strtolower($type[count($type) - 1]);
+    }
+
+    public function getCategory(): string
+    {
+        $type = explode('\\', $this->productable_type);
+        return preg_replace('/(?<!\ )[A-Z]/', ' $0', $type[count($type) - 1]);
+    }
+
+    public function getCategoryClass(): string
+    {
+        $type = explode('\\', $this->productable_type);
+        switch ($type[count($type) - 1]) {
+            case "DigitalProduct":
+                return "category-digital-text";
+            case "PhysicalProduct":
+                return "category-physical-text";
+            default:
+                return "";
+        }
+    }
 }
