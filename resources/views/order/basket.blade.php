4c31f3f2235ab49180af0d6ac67a1ecc640571bb
Nshan Vardanyan <Nshan.Vardanyan.1996@gmail.com>
2021-03-27 14:24:53 +0400

    create view to show basket

---
+++ resources/views/order/basket.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">{{ __('Store') }}</div>
+                    <div class="card-body">
+                        @include('_partials.product.allProducts', [
+                            'products' => $products,
+                            'showQuantity' => true,
+                            'paginate' => false
+                        ])
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
