145a6ce1735b770d2b80f87f96fe25e3860d6e10
Nshan Vardanyan <Nshan.Vardanyan.1996@gmail.com>
2021-03-28 06:18:59 +0400

    remove unnecessary script

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,8 +10,6 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-
 mix.js('resources/js/app.js', 'public/js')
-    .js('resources/js/basket_store.js', 'public/js/basket_store.js')
     .vue()
     .sass('resources/sass/app.scss', 'public/css');
