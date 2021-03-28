ef97796c72b7e47bae74377f8ac53f1df2e594fb
Nshan Vardanyan <Nshan.Vardanyan.1996@gmail.com>
2021-03-28 12:34:40 +0400

    create calculation services

---
+++ app/Services/CalculateWithoutTax.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace App\Services;
+
+
+use App\Interfaces\CalculateInterface;
+
+class CalculateWithoutTax implements CalculateInterface
+{
+    const TAX = 10;
+    public $products;
+    public $quantities;
+
+    public function setProducts($products)
+    {
+        $this->products = $products;
+    }
+
+    public function setQuantities($quantities)
+    {
+        $this->quantities = $quantities;
+    }
+
+    public function getCalculator(): CalculateWithTax
+    {
+        return $this;
+    }
+}
