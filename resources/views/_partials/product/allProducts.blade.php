4a603b35e7bef65da90b604cc4bea3aa19d5580d
Nshan Vardanyan <Nshan.Vardanyan.1996@gmail.com>
2021-03-28 06:04:24 +0400

    create optional parameters for partial views

--- resources/views/_partials/product/allProducts.blade.php
+++ resources/views/_partials/product/allProducts.blade.php
@@ -4,10 +4,17 @@
     </div>
 @endif
 <div class="row d-flex justify-content-around">
-    @foreach($products as $product)
+    @foreach($products as $index => $product)
         <div class="col-12 col-sm-8 col-md-6 col-lg-4 mb-3">
             <div class="card product-card">
-                @include($product->getViewPartialPath(), ['product' => $product])
+                @include($product->getViewPartialPath(), [
+                    "product" => $product,
+                    "showRemove" => $showRemove ?? false,
+                    "showAddToBasket" => $showAddToBasket ?? true,
+                    "quantities" => $quantities ?? [],
+                    "index" => $index,
+                    "action" => $action ?? "buy"
+                ])
             </div>
         </div>
     @endforeach
