3b362230a1f1362cc2020a3c7b30b11fd0186123
Nshan Vardanyan <Nshan.Vardanyan.1996@gmail.com>
2021-03-28 06:09:46 +0400

    create optional parameters for partial views, add checkout page link

--- resources/views/order/basket.blade.php
+++ resources/views/order/basket.blade.php
@@ -4,12 +4,21 @@
         <div class="row justify-content-center">
             <div class="col-md-12">
                 <div class="card">
-                    <div class="card-header">{{ __('Store') }}</div>
+                    <div class="card-header d-flex justify-content-between align-items-center">
+                        <div>{{ __('Basket') }}</div>
+                        <div>
+                            <a class="btn btn-primary" href="{{ route("checkout") }}">
+                                {{ __('Checkout') }}
+                            </a>
+                        </div>
+                    </div>
                     <div class="card-body">
                         @include('_partials.product.allProducts', [
-                            'products' => $products,
-                            'showQuantity' => true,
-                            'paginate' => false
+                            "products" => $products,
+                            "showRemove" => true,
+                            "showAddToBasket" => true,
+                            "paginate" => false,
+                            "action" => "update"
                         ])
                     </div>
                 </div>
