993d6415063dd518431c1b1ce84c732fb1fa8515
Nshan Vardanyan <Nshan.Vardanyan.1996@gmail.com>
2021-03-28 12:36:36 +0400

    create order service for order actions

---
+++ app/Services/OrderService.php
@@ -0,0 +1,66 @@
+<?php
+
+
+namespace App\Services;
+
+
+use App\Models\Order;
+use App\Models\OrderProduct;
+
+class OrderService
+{
+    public function getCalculator($calculationType)
+    {
+        switch ($calculationType) {
+            case "without_tax":
+                return new CalculateWithoutTax();
+            default:
+                return new CalculateWithTax();
+        }
+    }
+
+    public function calculate($calculator): array
+    {
+        $order = [
+            "products" => [],
+            "sum" => 0,
+            "tax" => $calculator::TAX
+        ];
+        foreach ($calculator->products as $index => $product) {
+            $product->quantity = $calculator->quantities[$index];
+            $price = $this->getPriceWithTax($product->quantity, $product->price, $calculator::TAX);
+            $product->sum = round($price, 2,);
+            $order["sum"] = round($order["sum"] + $product->sum, 2);
+            $order["products"][] = $product;
+        }
+        return $order;
+    }
+
+    public function store($orderDetails, $payment)
+    {
+        if ($orderDetails["sum"] > 0) {
+            $order = Order::create([
+                "user_id" => auth()->id(),
+                "tax" => round($orderDetails["tax"]),
+                "sum" => round($orderDetails["sum"]),
+                "payment" => $payment
+            ]);
+            foreach ($orderDetails["products"] as $product) {
+                OrderProduct::create([
+                    "order_id" => $order->id,
+                    "product_id" => $product->id,
+                    "price" => $product->price,
+                    "quantity" => $product->quantity,
+                    "sum" => $product->sum
+                ]);
+            }
+            return $order;
+        }
+        return null;
+    }
+
+    public function getPriceWithTax($quantity, $price, $tax)
+    {
+        return ((int) $quantity * floatval($price)) + (floatval($price) * (int) $tax / 100);
+    }
+}
