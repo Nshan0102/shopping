df955329d713b2a1584dea77fad8e57120489fac
Nshan Vardanyan <Nshan.Vardanyan.1996@gmail.com>
2021-03-28 12:33:13 +0400

    add actions to control orders

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,11 +2,66 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
+use App\Services\BasketService;
+use App\Services\OrderService;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
-    public function checkout(){
-        dd("checkout");
+    protected $calculator;
+    protected $orderService;
+    protected $basketService;
+
+    public function __construct(OrderService $orderService, BasketService $basketService)
+    {
+        parent::__construct();
+        $this->orderService = $orderService;
+        $this->basketService = $basketService;
+    }
+
+    public function checkout()
+    {
+        $this->setCalculator(auth()->user());
+        return view("order.checkout", ["order" => $this->getOrder()]);
+    }
+
+    public function store(Request $request, $payment)
+    {
+        if (!in_array($payment, Order::PAYMENT_METHODS)) {
+            return redirect('home');
+        }
+        $orderDetails = $this->getOrder();
+        $order = $this->orderService->store($orderDetails, $request->payment);
+        if (!$order) {
+            return redirect('home');
+        }
+        $this->basketService->setProducts([]);
+        return view("order.history", ["orders" => auth()->user()->orders]);
+    }
+
+    public function history()
+    {
+        return view("order.history", ["orders" => auth()->user()->orders]);
+    }
+
+    public function details(Order $order)
+    {
+        $orderWithAllRelations = Order::where("id", $order->id)
+            ->with('products.product.productable')->first();
+        return view('order.details', ["order" => $orderWithAllRelations]);
+    }
+
+    private function setCalculator($user)
+    {
+        $this->calculator = $this->orderService->getCalculator($user->calculation_type);
+    }
+
+    private function getOrder(): array
+    {
+        $this->setCalculator(auth()->user());
+        $this->calculator->setProducts($this->basketService->getProductsByBasket());
+        $this->calculator->setQuantities($this->basketService->getProductQuantities());
+        return $this->orderService->calculate($this->calculator);
     }
 }
