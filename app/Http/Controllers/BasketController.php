08bf3aec65bfa3f9cae4bad28bdb108f2f27a528
Nshan Vardanyan <Nshan.Vardanyan.1996@gmail.com>
2021-03-28 06:10:42 +0400

    create basket actions

--- app/Http/Controllers/BasketController.php
+++ app/Http/Controllers/BasketController.php
@@ -2,13 +2,65 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\BasketRemoveRequest;
+use App\Http\Requests\BasketSetRequest;
+use App\Http\Requests\BasketStoreRequest;
 use App\Models\Product;
-use Illuminate\Http\Request;
+use App\Services\BasketService;
 
 class BasketController extends Controller
 {
-    public function index(Request $request)
+    protected $basketService;
+
+    public function __construct(BasketService $basketService)
     {
-        return view('order.basket', ['products' => Product::with('productable')->limit(12)->get()]);
+        parent::__construct();
+        $this->basketService = $basketService;
+    }
+
+    public function index()
+    {
+        $products = Product::with("productable")
+            ->whereIn("id", $this->basketService->getProductIds())->get();
+        $quantities = $this->basketService->getProductQuantities();
+        return view("order.basket", [
+            "products" => $products,
+            "quantities" => $quantities
+        ]);
+    }
+
+    public function set(BasketSetRequest $request)
+    {
+        $basket = $request->products;
+        $this->basketService->setProducts($basket);
+        return session()->get("basket");
+    }
+
+    public function add(BasketStoreRequest $request)
+    {
+        $basket = session()->get("basket");
+        $found = false;
+        $product = $this->basketService->makeProduct($request->product);
+        $this->basketService->changeProductQuantity($basket, $product, $found, "add");
+        $this->basketService->pushProduct($basket, $product, $found);
+        $this->basketService->setProducts($basket);
+        return session()->get("basket");
+    }
+
+    public function update(BasketStoreRequest $request)
+    {
+        $basket = session()->get("basket");
+        $product = $this->basketService->makeProduct($request->product);
+        $this->basketService->changeProductQuantity($basket, $product, $found, "update");
+        $this->basketService->setProducts($basket);
+        return session()->get("basket");
+    }
+
+    public function remove(BasketRemoveRequest $request)
+    {
+        $basket = session()->get("basket");
+        $this->basketService->removeProduct($basket, (int) $request->product["id"]);
+        $this->basketService->setProducts($basket);
+        return session()->get("basket");
     }
 }
