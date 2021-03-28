66510293588c37df0659d41a8d14b49830ce7e45
Nshan Vardanyan <Nshan.Vardanyan.1996@gmail.com>
2021-03-28 12:35:27 +0400

    create view for order history

---
+++ resources/views/order/history.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <table class="table mb-0">
+                <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Products Count</th>
+                    <th scope="col">SUM</th>
+                    <th scope="col">Date</th>
+                    <th scope="col">Details</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($orders as $order)
+                    <tr>
+                        <th scope="row">{{ $order->id }}</th>
+                        <th>{{ $order->products->count() }}</th>
+                        <th>{{ $order->sum }}</th>
+                        <th>{{ $order->created_at }}</th>
+                        <th>
+                            <a href="{{ route("order.details", ["order" => $order]) }}">
+                                <i class="fas fa-eye"></i>
+                            </a>
+                        </th>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+@endsection
