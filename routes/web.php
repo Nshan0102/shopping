a35a989e6096528f6bf35adaeb043b1e5da50b04
Nshan Vardanyan <Nshan.Vardanyan.1996@gmail.com>
2021-03-28 12:32:45 +0400

    add new routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\BasketController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,15 +24,21 @@ Route::get('/', function () {
 Auth::routes();
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
-Route::group(['middleware' => 'auth'], function (){
-    Route::group(['prefix' => 'basket'], function (){
+Route::group(['middleware' => 'auth'], function () {
+    Route::group(['prefix' => 'basket'], function () {
         Route::get('/', [BasketController::class, 'index'])->name('basket.show');
         Route::post('/set', [BasketController::class, 'set']);
         Route::post('/add', [BasketController::class, 'add']);
         Route::put('/update', [BasketController::class, 'update']);
         Route::delete('/remove', [BasketController::class, 'remove']);
     });
-    Route::get('/checkout', [OrderController::class, 'checkout'])->name('checkout');
+    Route::group(['prefix' => 'order'], function () {
+        Route::get('/checkout', [OrderController::class, 'checkout'])->name('checkout');
+        Route::get('/pay/{payment}', [OrderController::class, 'store'])->name('order.pay');
+        Route::get('/history', [OrderController::class, 'history'])->name('order.history');
+        Route::get('/{order}/details', [OrderController::class, 'details'])->name('order.details');
+    });
+    Route::get('/product/{product}', [ProductController::class, 'show'])->name('product.show');
 });
 
 
